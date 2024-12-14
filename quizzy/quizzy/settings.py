5c3585c380a3e3373edd13d9c32a58be2d8be723
Vamfire <pantvaibhav16@gmail.com>
2024-12-14 15:18:54 +0530

    added local host

--- quizzy/quizzy/settings.py
+++ quizzy/quizzy/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-vch^d5864ymy!3^b@ui8bf-pu)8#s66y16bp21txat(_v_v%jc
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['quizzy-1-3rz6.onrender.com',]
+ALLOWED_HOSTS = ['quizzy-1-3rz6.onrender.com', '127.0.0.1', 'localhost',]
 
 
 # Application definition
